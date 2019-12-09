96502622c078565ce1270eb4beedb8842ebebfb9
Jeremie Dimino <jeremie@dimino.org>
2019-12-09 07:57:21 +0000

    Upgrade
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- test/test.ml
+++ test/test.ml
@@ -3,9 +3,7 @@ let ( let+ ) x f = `Let (x, f)
 let ( and+ ) a b = `And (a, b)
 
 let t =
-  let+ x = 1
-  and+ y = 2
-  and+ z = 3 in
+  let+ x = 1 and+ y = 2 and+ z = 3 in
   (x, y, z)
 
 let () =
@@ -22,12 +20,8 @@ let ( and+ ) a b = (a, b)
 let () =
   let q1 = Queue.create () in
   let q2 = Queue.create () in
-  let () = Queue.add 1 q1
-  and () = Queue.add 2 q1
-  and () = Queue.add 3 q1 in
-  let+ () = Queue.add 1 q2
-  and+ () = Queue.add 2 q2
-  and+ () = Queue.add 3 q2 in
+  let () = Queue.add 1 q1 and () = Queue.add 2 q1 and () = Queue.add 3 q1 in
+  let+ () = Queue.add 1 q2 and+ () = Queue.add 2 q2 and+ () = Queue.add 3 q2 in
   let l1 = Queue.fold (fun l x -> x :: l) [] q1 in
   let l2 = Queue.fold (fun l x -> x :: l) [] q2 in
   assert (l1 = l2)
